0217b07b8edccc37eabd2edbab1ca15271f17854
JamesDansie <jdansie8336@gmail.com>
2020-05-11 14:37:01 -0700

    added regex to find urls and store them in a list

--- src/main/java/facebook/Connection.java
+++ src/main/java/facebook/Connection.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Connection {
     // practicing getting a response from a site
@@ -28,7 +32,28 @@ public class Connection {
         // printing out the http doc
         Scanner sc = new Scanner(input);
         while(sc.hasNextLine()){
-            System.out.println(sc.nextLine());
+            String nextLine = sc.nextLine();
+            String regexStr = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}";
+
+//            from; https://stackoverflow.com/questions/47713276/how-to-slice-particular-values-from-a-string-using-regular-expression-in-javascr
+            Pattern regex = Pattern.compile(regexStr);
+            Matcher matcher = regex.matcher(nextLine);
+
+            List<String> urls = new ArrayList<>();
+
+            System.out.println("matcher find? " + matcher.find());
+            while(matcher.find()){
+                urls.add(matcher.group());
+            }
+
+            System.out.println(urls);
+
+//            String lookingFor = ".com";
+//            // simple search for other .com urls
+//            if(nextLine.contains(lookingFor)){
+//                System.out.println("contains a .com");
+//                System.out.println(nextLine);
+//            }
         }
 
         con.disconnect();
